339b9f39a84e5b17cd46ebb3f884d8c42b52f541
jeet-sorathiya <jeetsorathiya304@gmail.com>
2023-10-23 10:21:13 +0530

    Update ajex.js

--- jQuery/Ajax/ajex.js
+++ jQuery/Ajax/ajex.js
@@ -1,82 +1,74 @@
 $(document).ready(function () {
-    const taskList = $("#tasks");
-    const taskInput = $("#task-input");
-    const addTaskButton = $("#add-task");
-    const apiUrl = "https://jsonplaceholder.typicode.com/todos";
+  const taskList = $("#tasks");
+  const taskInput = $("#task-input");
+  const addTaskButton = $("#add-task");
+  const apiUrl = "https://jsonplaceholder.typicode.com/todos";
 
-    // fetch tasks
-    function fetchTasks() {
-      $.ajax({
-        url: apiUrl,
-        method: "GET",
-        success: function (data) {
+  // Fetch tasks
+  function fetchTasks() {
+      $.get(apiUrl, function (data) {
           taskList.empty();
           data.forEach(function (task) {
-            const li = $("<li>").html(
-              '<span>' + task.title + '</span><button class="delete" data-id="' + task.id + '">Delete</button>'
-            );
-            taskList.append(li);
+              const li = $("<li>").html(
+                  '<span>' + task.title + '</span><button class="delete" data-id="' + task.id + '">Delete</button>'
+              );
+              taskList.append(li);
           });
-        },
-        error: function (error) {
+      })
+      .fail(function (error) {
           console.error("Error fetching tasks:", error);
-        },
       });
-    }
+  }
 
-    fetchTasks();
+  fetchTasks();
 
-    // task deletion
-    function deleteTask(id) {
+  // Task deletion
+  function deleteTask(id) {
       $.ajax({
-        url: apiUrl + "/" + id,
-        method: "DELETE",
-        success: function () {
-          const taskElement = $('[data-id="' + id + '"]');
-          if (taskElement) {
-            taskElement.parent().remove();
-          }
-        },
-        error: function (error) {
-          console.error("Error deleting task:", error);
-        },
-      });
-    }
-
-    // adding a new task
-    addTaskButton.click(function () {
-      const text = taskInput.val();
-      if (text) {
-        const newTask = {
-          title: text,
-          completed: false,
-        };
-       console.log(`new task : ${newTask.title} will be add`);
-        $.ajax({
-          url: apiUrl,
-          method: "POST",
-          data: JSON.stringify(newTask),
-          contentType: "application/json; charset=UTF-8",
-          success: function (newTask) {
-            const li = $("<li>").html(
-              '<span>' + newTask.title + '</span><button class="delete" data-id="' + newTask.id + '">Delete</button>'
-            );
-            taskList.prepend(li);
-            taskInput.val("");
-            console.log(`new task : ${newTask.title} is now added`);
-
+          url: apiUrl + "/" + id,
+          method: "DELETE",
+          success: function () {
+              const taskElement = $('[data-id="' + id + '"]');
+              if (taskElement) {
+                  taskElement.parent().remove();
+              }
           },
           error: function (error) {
-            console.error("Error adding task:", error);
+              console.error("Error deleting task:", error);
           },
-        });
-      }
-    });
+      });
+  }
 
-    // Event listener for task deletion
-    taskList.on("click", ".delete", function () {
-      const id = $(this).attr("data-id");
-      console.log(`id is :- ${id}`);
-      deleteTask(id);
-    });
+  // Adding a new task
+  addTaskButton.click(function () {
+      const text = taskInput.val();
+      if (text) {
+          const newTask = {
+              title: text,
+              completed: false,
+          };
+          console.log(`Adding a new task: ${newTask.title}`);
+
+          $.post(apiUrl, newTask, function (Task) {
+              const li = $("<li>").html(
+                  '<span>' + Task.title + '</span><button class="delete" data-id="' + Task.id + '">Delete</button>'
+              );
+              taskList.prepend(li);
+              taskInput.val(""); // Clear the input field.
+              console.log(`New task: ${Task.title} is now added`);
+          })
+          .done(function () {
+              console.log("POST request succeeded.");
+          })
+          .fail(function (error) {
+              console.error("Error adding task:", error);
+          });
+      }
   });
+
+  taskList.on("click", ".delete", function () {
+      const id = $(this).attr("data-id");
+      console.log(`Deleting task with ID: ${id}`);
+      deleteTask(id);
+  });
+});
