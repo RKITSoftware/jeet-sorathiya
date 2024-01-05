03d764a1f3e2659c188e41be1189a58893b21300
jeet-sorathiya <jeetsorathiya304@gmail.com>
2024-01-05 17:54:31 +0530

    Static_class

---
+++ API Basic Training/Part_2_Basics _of_C#/7.2_Static_class/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace _7._2_Static_class
+{
+    /// <summary>
+    /// Class MathOperations
+    ///         type : static
+    ///         
+    ///         Add()
+    ///             type : static
+    ///             <param>int</param>
+    ///             <param>int</param>
+    ///             <return>int</return>
+    ///         Subtract()
+    ///             type : static
+    ///             <param>int</param>
+    ///             <param>int</param>
+    ///             <return>int</return>
+    /// </summary>
+    #region staic class
+    // Static Class
+    public static class MathOperations
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+    }
+    #endregion
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region staic class
+            // Static Class
+            Console.Write("Enter number 1: ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter number 2: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            int sum = MathOperations.Add(num1, num2);
+            Console.WriteLine($"Sum: {sum}");
+
+            #endregion
+        }
+    }
+}
