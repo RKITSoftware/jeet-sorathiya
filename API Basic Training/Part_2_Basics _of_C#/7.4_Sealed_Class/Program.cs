5e10fedbe5581dccfafb4e059997ccb2904108e1
jeet-sorathiya <jeetsorathiya304@gmail.com>
2024-01-05 17:56:17 +0530

    Sealed_Class

---
+++ API Basic Training/Part_2_Basics _of_C#/7.4_Sealed_Class/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _7._4_Sealed_Class
+{
+    /// <summary>
+    /// class SealedClass
+    ///         type : sealed
+    ///         
+    ///         DisplayMessage()
+    ///             type : void
+    ///             <param></param>
+    /// 
+    /// </summary>
+    #region Sealed Class
+    // Sealed Class
+    public sealed class SealedClass
+    {
+        public void DisplayMessage()
+        {
+            Console.WriteLine("This is a sealed class.");
+        }
+    }
+
+    #endregion
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region  Sealed Class
+            // Sealed Class
+            SealedClass sealedInstance = new SealedClass();
+            sealedInstance.DisplayMessage();
+            #endregion
+        }
+    }
+}
