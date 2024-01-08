9431c9c560801826526644419be0df7cda0757de
jeet-sorathiya <jeetsorathiya304@gmail.com>
2024-01-08 16:05:46 +0530

    referance

---
+++ API Basic Training/Part_2_Basics _of_C#/23_Reference_to_Assemblies/Program.cs
@@ -0,0 +1,14 @@
+﻿using _14_Scope_Accessibility_modifier;
+
+namespace _23_Reference_to_Assemblies
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PublicClass objofPublicClass = new PublicClass();
+            objofPublicClass.PublicMethod();
+
+        }
+    }
+}
