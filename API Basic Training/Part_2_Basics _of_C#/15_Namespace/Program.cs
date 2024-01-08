091ce97d0a7591d546908b0d634ab40d5a423090
jeet-sorathiya <jeetsorathiya304@gmail.com>
2024-01-08 13:24:14 +0530

    namespace

---
+++ API Basic Training/Part_2_Basics _of_C#/15_Namespace/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _15_Namespace
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Trainee Name : ");
+            string name = Console.ReadLine();
+            Trainee objofTrainee = new Trainee(name); // Trainee Class is in same namespace
+        }
+    }
+}
---
+++ API Basic Training/Part_2_Basics _of_C#/15_Namespace/Trainee.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _15_Namespace
+{
+    internal class Trainee
+    {
+        public Trainee(string name)
+        {
+            Console.WriteLine($"{name} Work on C# Basic");
+        }
+    }
+}
