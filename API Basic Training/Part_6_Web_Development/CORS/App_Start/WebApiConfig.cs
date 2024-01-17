bc2368749fe213e3c1516c05408d8cb90604ff73
jeet-sorathiya <jeetsorathiya304@gmail.com>
2024-01-17 16:08:06 +0530

    CORS

---
+++ API Basic Training/Part_6_Web_Development/CORS/App_Start/WebApiConfig.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Http;
+
+namespace CORS
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Enable CORS for all origins, headers, and methods.
+            //  var cors = new EnableCorsAttribute("*", "*", "*");
+            // config.EnableCors(cors);
+            config.EnableCors();
+
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ API Basic Training/Part_6_Web_Development/CORS/Controllers/CorsController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+
+namespace CORS.Controllers
+{
+    /// <summary>
+    /// Controller for handling CORS-related requests.
+    /// </summary>
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class CorsController : ApiController
+    {
+        // Data to be returned by the API
+        public List<string> data = new List<string> { "Jeet", "Tony-Stark", "Captain-America", "SpiderMan" };
+
+        /// <summary>
+        /// Gets the entire list of data.
+        /// </summary>
+        [HttpGet]
+        public IHttpActionResult Get()
+        {
+            // Return the entire list of data
+            return Ok(data);
+        }
+
+        /// <summary>
+        /// Gets a specific item from the data list based on the provided ID.
+        /// </summary>
+        /// <param name="id">The ID of the item to retrieve.</param>
+        [HttpGet]
+        [DisableCors] // Disabling CORS for this specific action
+        public IHttpActionResult Get(int id)
+        {
+            if(id < 1 && id > data.Count)
+            {
+                return NotFound();
+            }
+            return Ok(data[id-1]);
+        }
+    }
+}
