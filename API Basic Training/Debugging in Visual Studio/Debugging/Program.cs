c1fea0c4c165b7069dd823786d04af0d70cbad70
jeet-sorathiya <jeetsorathiya304@gmail.com>
2024-01-26 16:37:33 +0530

    Debugging

---
+++ API Basic Training/Debugging in Visual Studio/Debugging/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Debugging
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int value1 = 10, value2 = 20;
+
+            int finalValue = value1 + value2;
+
+            for(int i = 0; i < finalValue; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
