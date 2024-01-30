b69d97a284ca28241345d22f52aabe772c595f14
jeet-sorathiya <jeetsorathiya304@gmail.com>
2024-01-30 15:44:40 +0530

    Lambda expression

---
+++ API Basic Training/C# Advance/LambdaExpression/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LambdaExpression
+{
+    /// <summary>
+    /// A simple program demonstrating the use of lambda expressions.
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Lambda expression with no parameters
+            Action name = () => Console.WriteLine("Jeet-Sorathiya");
+            name();
+
+            // Lambda expression with parameters
+            Func<int, int, double> add = (a, b) => a + b;
+            Console.WriteLine("Sum: " + add(4, 4));
+
+            // Lambda expression in a List
+            List<int> employeeId = new List<int> { 365, 366, 367, 368, 369, 370 };
+            var myId = employeeId.Find(id => id == 369);
+            Console.WriteLine("my Id: " + myId);
+
+        }
+    }
+}
