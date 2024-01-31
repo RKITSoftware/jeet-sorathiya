3c4dd14151fda8dc1b5c0fb12efb191e41c958e2
jeet-sorathiya <jeetsorathiya304@gmail.com>
2024-01-31 15:14:57 +0530

    Base_Library

---
+++ API Basic Training/C# Advance/Base_Library/MyList.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace MyList
+{
+    /// <summary>
+    /// Represents a custom list that extends the functionality of the generic List class
+    /// </summary>
+    /// <typeparam name="T">The type of elements in the list</typeparam>
+    public class MyList<T> : List<T>
+    {
+        /// <summary>
+        /// Initializes a new instance of the MyList class
+        /// </summary>
+        public MyList() : base() { }
+
+        /// <summary>
+        /// Adds an item to the list
+        /// </summary>
+        /// <param name="item">The item to be added</param>
+        public void AddToList(T item)
+        {
+            base.Add(item);
+        }
+
+        /// <summary>
+        /// Removes an item from the list
+        /// </summary>
+        /// <param name="item">The item to be removed</param>
+        public void RemoveToList(T item)
+        {
+            base.Remove(item);
+        }
+    }
+}
---
+++ API Basic Training/C# Advance/Base_Library/Program.cs
@@ -0,0 +1,32 @@
+﻿using MyList;
+using System;
+
+namespace Base_Library
+{
+    /// <summary>
+    /// A demonstration program using the MyList class
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating an instance of MyList with integer elements
+            MyList<int> list = new MyList<int>();
+
+            // Adding elements to the list
+            list.AddToList(1);
+            list.AddToList(2);
+            list.AddToList(3);
+            list.AddToList(4);
+
+            // Removing an element from the list
+            list.RemoveToList(1);
+
+            // Displaying the elements in the list using a foreach loop
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
