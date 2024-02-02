2c13d2a074ec5f3fa5a197e880b44d67855759b4
jeet-sorathiya <jeetsorathiya304@gmail.com>
2024-02-02 12:49:16 +0530

    dynamic type

---
+++ API Basic Training/C# Advance/Dynamic_type/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Dynamic_type
+{
+    /// <summary>
+    ///  demonstration of using dynamic type
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// Adds two dynamic items, allowing for flexibility in data types.
+        /// </summary>
+        /// <param name="item1">The first dynamic item</param>
+        /// <param name="item2">The second dynamic item</param>
+        /// <returns>The result of the addition operation</returns>
+        public static dynamic Add(dynamic item1, dynamic item2)
+        {
+            return (item1 + item2);
+            //return null;
+        }
+        static void Main(string[] args)
+        {
+            dynamic item1 = "Hello";
+            dynamic item2 = 10;
+
+            Console.WriteLine($"type of item1 : {item1.GetType()}");
+            Console.WriteLine($"type of item1 : {item2.GetType()}");
+            Console.WriteLine(Add(item2, item1));
+
+            // change value type
+            item1 = 20;
+            Console.WriteLine($"type of item1 : {item1.GetType()}");
+            Console.WriteLine(Add(item2, item1));
+        }
+    }
+}
