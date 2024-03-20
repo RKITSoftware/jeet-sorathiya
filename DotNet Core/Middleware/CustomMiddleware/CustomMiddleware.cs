c911c20a6895dbfdd51e6ec8493eac965f832973
jeet-sorathiya <jeetsorathiya304@gmail.com>
2024-03-20 17:24:15 +0530

    middleware

---
+++ DotNet Core/Middleware/CustomMiddleware/CustomMiddleware.cs
@@ -0,0 +1,25 @@
+﻿namespace Middleware.CustomMiddleware
+{
+    /// <summary>
+    /// CustomMiddleware for log info about request
+    /// </summary>
+    public class CustomMiddleware : IMiddleware
+    {
+        /// <summary>
+        /// asyc type InvokeAsync method for log info about request
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="next"></param>
+        /// <returns></returns>
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            Console.WriteLine($"Request Path : {context.Request.Path}");
+
+            Console.WriteLine($"Method Type : {context.Request.Method}");
+
+            await next(context); // call next middleware
+
+            Console.WriteLine($"Response Code : {context.Response.StatusCode}");
+        }
+    }
+}
---
+++ DotNet Core/Middleware/Startup.cs
@@ -0,0 +1,72 @@
+﻿using Middleware.CustomMiddleware;
+/// <summary>
+/// Class responsible for configuring the application during startup.
+/// </summary>
+public class Startup
+{
+    /// <summary>
+    /// Configuration property containing the application's configuration.
+    /// </summary>
+    public IConfiguration Configuration { get; }
+
+    /// <summary>
+    /// Constructor for the Startup class.
+    /// </summary>
+    /// <param name="configuration">The application's configuration.</param>
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    /// <summary>
+    /// Configures services required by the application.
+    /// </summary>
+    /// <param name="services">Collection of services to be configured.</param>
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers();
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+        services.AddSingleton<CustomMiddleware>(); // add customMiddleware
+    }
+
+    /// <summary>
+    /// Configures the HTTP request pipeline
+    /// </summary>
+    /// <param name="app">request pipeline</param>
+    /// <param name="environment">hosting environment</param>
+    public void Configure(WebApplication app, IWebHostEnvironment environment)
+    {
+        // Configure the HTTP request pipeline.
+        if (environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        // Redirect HTTP requests to HTTPS
+        app.UseHttpsRedirection();
+
+        // Authorize requests
+        app.UseAuthorization();
+
+        // Map incoming requests to controller actions
+        app.MapControllers();
+
+        app.MapGet("/api/resource", async context =>
+        {
+            await Task.Delay(10000); // Simulate an asynchronous operation
+            await context.Response.WriteAsync("Done");
+        });
+
+        // Call CustomMiddleware
+        app.UseMiddleware<CustomMiddleware>();
+
+        // StaticFile Middleware
+        app.UseStaticFiles();
+
+        // End the request pipeline
+        app.Run();
+
+    }
+}
