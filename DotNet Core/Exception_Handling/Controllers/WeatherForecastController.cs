102247302b723fcd203de84960f53f54f50323f5
jeet-sorathiya <jeetsorathiya304@gmail.com>
2024-03-29 16:53:00 +0530

    Exception Handling

---
+++ DotNet Core/Exception_Handling/Controllers/WeatherForecastController.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Exception_Handling.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public int Get()
+        {
+            int a = 10, b = 0;
+            return a/b;
+        }
+    }
+}
---
+++ DotNet Core/Exception_Handling/Startup.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using System.ComponentModel.Design.Serialization;
+using System.Net;
+/// <summary>
+/// Class responsible for configuring the application during startup.
+/// </summary>
+public class Startup
+{
+    /// <summary>
+    /// Configuration property containing the application's configuration.
+    /// </summary>
+    public IConfiguration Configuration { get; }
+
+    /// <summary>
+    /// Constructor for the Startup class.
+    /// </summary>
+    /// <param name="configuration">The application's configuration.</param>
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    /// <summary>
+    /// Configures services required by the application.
+    /// </summary>
+    /// <param name="services">Collection of services to be configured.</param>
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers();
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+    }
+
+    /// <summary>
+    /// Configures the HTTP request pipeline
+    /// </summary>
+    /// <param name="app">request pipeline</param>
+    /// <param name="environment">hosting environment</param>
+    public void Configure(WebApplication app, IWebHostEnvironment environment)
+    {
+        // Configure the HTTP request pipeline.
+        if (environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+            //app.UseDeveloperExceptionPage(new DeveloperExceptionPageOptions
+            //{
+            //    SourceCodeLineCount = 2 // number of source code lines
+            //});
+            app.UseExceptionHandler(
+               options =>
+               {
+                   options.Run(
+                       async context =>
+                       {
+                           context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                           var ex = context.Features.Get<IExceptionHandlerFeature>();
+                           if (ex != null)
+                           {
+                               await context.Response.WriteAsync(ex.Error.Message);
+                           }
+                       }
+                );
+               }
+            );
+        }
+
+        app.MapGet("/", async context =>
+        {
+            int Number1 = 10, Number2 = 0;
+            int Result = Number1 / Number2; //This statement will throw Runtime Exception
+            await context.Response.WriteAsync($"Result : {Result}");
+        });
+        // Redirect HTTP requests to HTTPS
+        app.UseHttpsRedirection();
+
+        // Authorize requests
+        app.UseAuthorization();
+
+        // Map incoming requests to controller actions
+        app.MapControllers();
+
+        // End the request pipeline
+        app.Run();
+
+    }
+}
