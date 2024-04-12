d02aec9918b5d8ef8311531b6a9365137f04f870
jeet-sorathiya <jeetsorathiya304@gmail.com>
2024-04-12 11:56:45 +0530

    Update : Code Style

--- API Basic Training/Part_6_Web_Development/Building_Web_API/BL/BLEmployee.cs
+++ API Basic Training/Part_6_Web_Development/Building_Web_API/BL/BLEmployee.cs
@@ -1,114 +1,103 @@
 ﻿using Building_Web_API.Helper_Methods;
 using Building_Web_API.Models;
 using System.Collections.Generic;
-using System.Web.Http;
 
-namespace Building_Web_API.Controllers
+namespace Building_Web_API.BL
 {
     /// <summary>
-    /// Controller for managing employee data in a Web API.
+    /// Business logic class for managing employee data.
     /// </summary>
-    public class EmployeeController : ApiController
+    public class BLEmployee
     {
         /// <summary>
         /// Static list to store sample employee data.
         /// </summary>
-        static List<Employee> employeesList = new List<Employee>
+        private static readonly List<Employee> employeesList = new List<Employee>
         {
-             // Sample employees with initial data
+            // Sample employees with initial data
             new Employee{EmployeeID = HelperMethods.newEmpID(), EmployeeName = "Jeet Sorathiya", EmployeeDesignation = "Full Stack Developer", EmployeePhone = "9123654789", EmployeeAge = 21, IsActive = true},
             new Employee{EmployeeID = HelperMethods.newEmpID(), EmployeeName = "Tony Stark", EmployeeDesignation = "Project Coordinator", EmployeePhone = "7456321589", EmployeeAge = 35, IsActive = false},
             new Employee{EmployeeID = HelperMethods.newEmpID(), EmployeeName = "Captain America", EmployeeDesignation = "HR", EmployeePhone = "9214536875", EmployeeAge = 100, IsActive = true},
             new Employee{EmployeeID = HelperMethods.newEmpID(), EmployeeName = "Spiderman", EmployeeDesignation = "Trainee", EmployeePhone = "6589321456", EmployeeAge = 18, IsActive = true},
         };
 
-
         /// <summary>
-        ///  GET : api/Employee, Retrieves all employees.
+        /// Retrieves all employees.
         /// </summary>
-        /// <returns>List of employees.</returns>
-        [HttpGet]
-        public IHttpActionResult Get()
+        /// <returns>Returns a collection of all employees.</returns>
+        public IEnumerable<Employee> Get()
         {
-            // Returning a response with the list of employees
-            return Ok(employeesList);
+            return employeesList;
         }
 
         /// <summary>
-        /// GET : api/Employee/{id}, Retrieves an employee by ID.
+        /// Retrieves an employee by ID.
         /// </summary>
-        /// <param name="id">Employee ID.</param>
-        /// <returns>Employee with the specified ID.</returns>
-        [HttpGet]
-        public IHttpActionResult Get(int id)
+        /// <param name="id">The ID of the employee to retrieve.</param>
+        /// <returns>Returns the employee with the specified ID.</returns>
+        public Employee GetByID(int id)
         {
-            // Returning a response with the employee matching the given ID
-            return Ok(employeesList.Find(employee => employee.EmployeeID == id));
+            return employeesList.Find(employee => employee.EmployeeID == id);
         }
 
         /// <summary>
-        /// POST : api/Employee, Adds a new employee
+        /// Adds a new employee.
         /// </summary>
-        /// <param name="employee">Employee data to be added.</param>
-        /// <returns>List of employees after adding the new employee.</returns>
-        [HttpPost]
-        public IHttpActionResult Post(Employee employee)
+        /// <param name="employee">The employee object to add.</param>
+        /// <returns>Returns true if the employee is successfully added; otherwise, false.</returns>
+        public bool Add(Employee employee)
         {
             // Generating a new employee ID and adding the new employee to the list
             employee.EmployeeID = HelperMethods.newEmpID();
             employeesList.Add(employee);
-            // Returning a response with the updated list of employees
-            return Ok(employeesList);
+            return true;
         }
 
         /// <summary>
-        ///  PUT : api/Employee/{id}, Updates an existing employee by ID.
+        /// Updates an employee by ID.
         /// </summary>
-        /// <param name="id">Employee ID.</param>
-        /// <param name="newEmployee">Updated employee data.</param>
-        /// <returns>Updated employee details.</returns>
-        [HttpPut]
-        public IHttpActionResult Put(int id, Employee newEmployee)
+        /// <param name="id">The ID of the employee to update.</param>
+        /// <param name="newEmployee">The updated employee object.</param>
+        /// <returns>Returns true if the employee is successfully updated; otherwise, false.</returns>
+        public bool Update(int id, Employee newEmployee)
         {
             // Finding the current employee in the list
             Employee currentEmployee = employeesList.Find(employee => employee.EmployeeID == id);
-            
+
             // Checking if the employee exists
             if (currentEmployee == null)
             {
-                //Console.WriteLine("Employee Not Found At this ID");
-                return NotFound();
+                return false;
             }
+
             // Updating employee details
             currentEmployee.EmployeeName = newEmployee.EmployeeName;
             currentEmployee.EmployeeDesignation = newEmployee.EmployeeDesignation;
             currentEmployee.EmployeePhone = newEmployee.EmployeePhone;
             currentEmployee.EmployeeAge = newEmployee.EmployeeAge;
 
-            // Returning a response with the updated employee
-            return Ok(currentEmployee);
+            return true;
         }
 
         /// <summary>
-        ///  DELETE : api/Employee/{id}, Deletes an employee by ID.
+        /// Deletes an employee by ID.
         /// </summary>
-        /// <param name="id">Employee ID.</param>
-        /// <returns>Deleted employee details.</returns>
-        [HttpDelete]
-        public IHttpActionResult Delete(int id)
+        /// <param name="id">The ID of the employee to delete.</param>
+        /// <returns>Returns true if the employee is successfully deleted; otherwise, false.</returns>
+        public bool Delete(int id)
         {
             // Finding the current employee in the list
             var deleteEmployee = employeesList.Find(employee => employee.EmployeeID == id);
+
             // Checking if the employee exists
             if (deleteEmployee == null)
             {
-                // Returning a Not Found response
-                return NotFound();
+                return false;
             }
+
             // Removing the employee from the list
             employeesList.Remove(deleteEmployee);
-            // Returning a response with the deleted employee
-            return Ok(deleteEmployee);
+            return true;
         }
     }
 }
---
+++ API Basic Training/Part_6_Web_Development/Building_Web_API/Controllers/CLEmployeeController.cs
@@ -0,0 +1,121 @@
+﻿using Building_Web_API.BL;
+using Building_Web_API.Models;
+using System.Web.Http;
+
+namespace Building_Web_API.Controllers
+{
+    /// <summary>
+    /// Controller for managing employee data in a Web API.
+    /// </summary>
+    [RoutePrefix("api/Employee")]
+    public class CLEmployeeController : ApiController
+    {
+        private readonly BLEmployee _blEmployee;
+
+        /// <summary>
+        /// Constructor for CLEmployeeController.
+        /// </summary>
+        public CLEmployeeController()
+        {
+            _blEmployee = new BLEmployee();
+        }
+
+        /// <summary>
+        /// Retrieves all employees.
+        /// </summary>
+        /// <returns>Returns a list of all employees.</returns>
+        [HttpGet]
+        [Route("Get")]
+        public IHttpActionResult Get()
+        {
+            // Returning a response with the list of employees
+            return Ok(_blEmployee.Get());
+        }
+
+        /// <summary>
+        /// Retrieves an employee by ID.
+        /// </summary>
+        /// <param name="id">The ID of the employee to retrieve.</param>
+        /// <returns>Returns the employee with the specified ID.</returns>
+        [HttpGet]
+        [Route("Get/{id}")]
+        public IHttpActionResult Get(int id)
+        {
+            // Returning a response with the employee matching the given ID
+            return Ok(_blEmployee.GetByID(id));
+        }
+
+        /// <summary>
+        /// Retrieves an employee by object.
+        /// </summary>
+        /// <param name="employee">The employee object to retrieve.</param>
+        /// <returns>Returns the retrieved employee object.</returns>
+        [HttpGet]
+        [Route("GetByObject")]
+        public IHttpActionResult Get([FromBody] Employee employee)
+        {
+            if (employee != null)
+            {
+                return Ok(employee);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        /// <summary>
+        /// Adds a new employee.
+        /// </summary>
+        /// <param name="employee">The employee object to add.</param>
+        /// <returns>Returns the added employee object.</returns>
+        [HttpPost]
+        [Route("AddEmployee")]
+        public IHttpActionResult Post(Employee employee)
+        {
+            if (_blEmployee.Add(employee))
+            {
+                return Ok(employee);
+            }
+            else
+            {
+                return InternalServerError();
+            }
+        }
+
+
+        /// <summary>
+        /// Updates an employee by ID.
+        /// </summary>
+        /// <param name="id">The ID of the employee to update.</param>
+        /// <param name="newEmployee">The updated employee object.</param>
+        /// <returns>Returns an HTTP status code indicating the success of the operation.</returns>
+        [HttpPut]
+        [Route("UpdateEmployee/{id}")]
+        public IHttpActionResult Put(int id, Employee newEmployee)
+        {
+            if (_blEmployee.Update(id, newEmployee))
+            {
+                return Ok();
+            }
+            return InternalServerError();
+        }
+
+
+        /// <summary>
+        /// Deletes an employee by ID.
+        /// </summary>
+        /// <param name="id">The ID of the employee to delete.</param>
+        /// <returns>Returns an HTTP status code indicating the success of the operation.</returns>
+        [HttpDelete]
+        [Route("DeleteEmployee/{id}")]
+        public IHttpActionResult Delete(int id)
+        {
+            if (_blEmployee.Delete(id))
+            {
+                return Ok();
+            }
+            return InternalServerError();
+        }
+    }
+}
