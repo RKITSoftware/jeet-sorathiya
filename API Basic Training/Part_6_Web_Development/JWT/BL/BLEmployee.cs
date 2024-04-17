3b8e58463710f2061eb77dd829ce120e4e352ed1
jeet-sorathiya <jeetsorathiya304@gmail.com>
2024-04-17 15:19:13 +0530

    code change

---
+++ API Basic Training/Part_6_Web_Development/JWT/BL/BLEmployee.cs
@@ -0,0 +1,30 @@
+﻿using JWT.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace JWT.BL
+{
+    public class BLEmployee
+    {
+        /// <summary>
+        /// Static list of employees
+        /// </summary>
+        static readonly List<Employee> employeeList = new List<Employee>
+        {
+            new Employee{EmployeeName = "Jeet",Password = "1234"},
+            new Employee{EmployeeName = "Tony-Stark", Password = "1234"},
+            new Employee{EmployeeName = "SpiderMan", Password = "1234"},
+            new Employee{EmployeeName = "hokai", Password = "1234"},
+        };
+
+        /// <summary>
+        /// Checks if the provided employee exists in the employee list.
+        /// </summary>
+        /// <param name="employee">The employee to check.</param>
+        /// <returns>True if the employee exists in the list, otherwise false.</returns>
+        public bool IsEmployee(Employee employee)
+        {
+            return employeeList.Any(emp => emp.EmployeeName == employee.EmployeeName && emp.Password == employee.Password);
+        }
+    }
+}
--- API Basic Training/Part_6_Web_Development/JWT/Controllers/CLEmployeeController.cs
+++ API Basic Training/Part_6_Web_Development/JWT/Controllers/CLEmployeeController.cs
@@ -1,7 +1,5 @@
 ﻿using JWT.BL;
 using JWT.Models;
-using System.Collections.Generic;
-using System.Linq;
 using System.Web.Http;
 
 namespace JWT.Controllers
@@ -13,20 +11,13 @@ namespace JWT.Controllers
     public class CLEmployeeController : ApiController
     {
         // Business logic class for JWT token generation and verification
-        private BLJWTToken objBLJWTToken;
-
-        /// <summary>
-        /// Static list of employees
-        /// </summary>
-        public static List<Employee> employeeList = new List<Employee>
+         BLJWTToken _objBLJWTToken;
+        BLEmployee _objBLEMPloyee;
+        public CLEmployeeController()
         {
-            new Employee{EmployeeName = "Jeet",Password = "1234"},
-            new Employee{EmployeeName = "Tony-Stark", Password = "1234"},
-            new Employee{EmployeeName = "SpiderMan", Password = "1234"},
-            new Employee{EmployeeName = "hokai", Password = "1234"},
-
-
-        };
+            _objBLJWTToken = new BLJWTToken();
+            _objBLEMPloyee = new BLEmployee();
+        }
 
         /// <summary>
         /// Generates a JWT token for the provided employee credentials.
@@ -38,18 +29,12 @@ namespace JWT.Controllers
         public IHttpActionResult JWTToken(Employee employee)
         {
             // Check if the employee exists in the static list
-            bool isEmployee = employeeList.Any(emp => emp.EmployeeName == employee.EmployeeName && emp.Password == employee.Password);
-            if (!isEmployee)
-            {
+            if (!_objBLEMPloyee.IsEmployee(employee))            {
                 // Return Unauthorized if the employee is not found
                 return Unauthorized();
             }
-
-            // Initialize the BLJWTToken object for token generation
-            objBLJWTToken = new BLJWTToken();
-
             // Generate JWT token for the employee
-            string token = objBLJWTToken.GenerateToken(employee.EmployeeName);
+            string token = _objBLJWTToken.GenerateToken(employee.EmployeeName);
 
             // Return the generated token
             return Ok(token);
@@ -64,11 +49,8 @@ namespace JWT.Controllers
         [HttpGet]
         public IHttpActionResult VerifyJWT(string token)
         {
-            // Initialize the BLJWTToken object for token verification
-            objBLJWTToken = new BLJWTToken();
-
             // Verify the JWT token and extract the employee name
-            string employeeName = objBLJWTToken.VerifyToken(token);
+            string employeeName = _objBLJWTToken.VerifyToken(token);
             if (employeeName == null)
             {
                 // Return Unauthorized if token verification fails
