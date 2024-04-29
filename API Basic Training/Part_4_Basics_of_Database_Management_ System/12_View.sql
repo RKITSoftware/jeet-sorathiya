f06d9d5f7c2c7769b9a95450d1231af17792b8a5
jeet-sorathiya <jeetsorathiya304@gmail.com>
2024-04-29 12:51:57 +0530

    Update

--- API Basic Training/Part_4_Basics_of_Database_Management_ System/12_View.sql
+++ API Basic Training/Part_4_Basics_of_Database_Management_ System/12_View.sql
@@ -1,12 +1,13 @@
 -- View
-CREATE VIEW employee_view  AS 
+CREATE VIEW vws_employee_view  AS 
 SELECT P02F01,P02F02
 FROM emp02
 WHERE 	P02F03 = 2;
 
 -- SELECT From View
-SELECT P02F02 FROM employee_view ;
+SELECT P02F02 FROM vws_employee_view ;
 
 -- Drop View
-DROP VIEW employee_view ;
+DROP VIEW vws_employee_view ;
+-- vws_emp01
 
