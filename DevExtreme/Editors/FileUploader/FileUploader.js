4696fda344dd0fc2ae251bffbc50fb7d71c34738
jeet-sorathiya <jeetsorathiya304@gmail.com>
2024-05-23 18:03:48 +0530

    file uploader

--- DevExtreme/Editors/FileUploader/FileUploader.js
+++ DevExtreme/Editors/FileUploader/FileUploader.js
@@ -1,1 +1,13 @@
-﻿
+﻿$(function () {
+
+    $("#imgUploader").dxFileUploader({
+        abortUpload: function (file, uploadInfo) {
+            console.log(file);
+            console.log(uploadInfo);
+        },
+        accept:".png,.txt",
+      //  allowedFileExtensions: ['.jpg', '.jpeg', '.gif', '.png'],
+       // allowCanceling: false
+    });
+
+});
