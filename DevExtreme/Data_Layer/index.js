9acf154f7edb20d4a77b90049ff02fc86bf282cd
jeet-sorathiya <jeetsorathiya304@gmail.com>
2024-06-10 10:48:31 +0530

    Upload DataLayer

---
+++ DevExtreme/Data_Layer/index.js
@@ -0,0 +1,16 @@
+﻿$(function () {
+
+    var store = new DevExpress.data.ArrayStore({
+        data: [
+            { id: 1, name: "Jeet" },
+            { id: 2, name: "Tony Stark" },
+        ]
+    });
+
+    $("#div1").dxSelectBox({
+        dataSource: store,
+        valueExpr: "id",
+        displayExpr: "name",
+    });
+
+});
