2873188d7959af8603fecd539741be6599c917ec
jeet-sorathiya <jeetsorathiya304@gmail.com>
2024-06-14 17:15:19 +0530

    Exception Handling

--- DotNet Core/Exception_Handling/Startup.cs
+++ DotNet Core/Exception_Handling/Startup.cs
@@ -43,26 +43,30 @@ public class Startup
         {
             app.UseSwagger();
             app.UseSwaggerUI();
-            //app.UseDeveloperExceptionPage(new DeveloperExceptionPageOptions
-            //{
-            //    SourceCodeLineCount = 2 // number of source code lines
-            //});
+            app.UseDeveloperExceptionPage(new DeveloperExceptionPageOptions
+            {
+                SourceCodeLineCount = 3 // number of source code lines
+            });
+           
+        }
+        else
+        {
             app.UseExceptionHandler(
-               options =>
-               {
-                   options.Run(
-                       async context =>
-                       {
-                           context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
-                           var ex = context.Features.Get<IExceptionHandlerFeature>();
-                           if (ex != null)
-                           {
-                               await context.Response.WriteAsync(ex.Error.Message);
-                           }
-                       }
-                );
-               }
-            );
+              options =>
+              {
+                  options.Run(
+                      async context =>
+                      {
+                          context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                          var ex = context.Features.Get<IExceptionHandlerFeature>();
+                          if (ex != null)
+                          {
+                              await context.Response.WriteAsync(ex.Error.Message);
+                          }
+                      }
+               );
+              }
+           );
         }
 
         app.MapGet("/", async context =>
