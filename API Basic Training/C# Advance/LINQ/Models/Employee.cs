d4fa18fb5a543868d9e990f951012fedd2eb189d
jeet-sorathiya <jeetsorathiya304@gmail.com>
2024-06-14 17:18:52 +0530

    Code Update

--- API Basic Training/C# Advance/LINQ/Models/Employee.cs
+++ API Basic Training/C# Advance/LINQ/Models/Employee.cs
@@ -11,9 +11,25 @@ namespace LINQ.Models
     /// </summary>
     public class Employee
     {
+        /// <summary>
+        /// Gets or sets the ID of the employee.
+        /// </summary>
         public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the employee.
+        /// </summary>
         public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the age of the employee.
+        /// </summary>
         public int Age { get; set; }
+
+        /// <summary>
+        /// Gets or sets the department of the employee.
+        /// </summary>
         public string Department { get; set; }
     }
+
 }
